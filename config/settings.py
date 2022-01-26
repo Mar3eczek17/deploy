d7789b8cc6f05399ac0d8fcec838b37c1d5b5e79
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2022-01-26 19:11:35 +0100

    heroku config

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rptbacvz2+-3t2#guo0(4uh(w#8a1xan8bpz!xj!ou5-@fzx6r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
